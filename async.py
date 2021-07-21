d50f9835728331a6d680673784f48542eb2f121b
Arle89 <anarhyarlekin@gmail.com>
2021-07-21 15:19:57 +0300

    add pika

--- async.py
+++ async.py
@@ -1,17 +1,32 @@
 import asyncio
 import time
+import pika
 
 async def say_after(delay, what):
     await asyncio.sleep(delay)
-    await print(what)
+    print(what)
 
 
 
 async def main():
     print (f"started at : {time.strftime('%X')}")
 
-    await say_after(2, "hello")
-    await say_after(4, "world")
+    task1 = asyncio.create_task(say_after(1, "hello"))
+    task2 = asyncio.create_task(say_after(2, "world"))
+
+
+    await task1
+    await task2
 
     print (f"Finshed at : {time.strftime('%X')}")
 
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue = 'hello')
+channel.basic_publish(exchange='', routing_key='hello', body='Hello world!')
+print ('Hello world')
+connection.close()
+
+
+asyncio.run(main())
